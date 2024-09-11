371473c6de3db2515bdb3e77d2d474da2a94b57d
Dylann-Nick <147245156+Dylann-Nick@users.noreply.github.com>
2024-09-11 10:22:09 +0200

    Ajout du controller de Resevation d'espace

---
+++ DI2P1G6.API/Controllers/BookingController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DI2P1G6.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+    }
+}
